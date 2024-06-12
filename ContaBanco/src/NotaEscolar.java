b902f93a765d1c8ff7970bc7421e7cfa883e06a7
Paulo Sergio <ashnikko5297@gmail.com>
2024-06-12 07:43:21 -0300

    Estruturas condicionais ternárias, são usuais no dia a dia.

--- ContaBanco/src/NotaEscolar.java
+++ ContaBanco/src/NotaEscolar.java
@@ -1,10 +1,7 @@
 public class NotaEscolar {
 
     public static void main(String[] args) {
-        int nota = 6;
-		//String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
-        //System.out.println(resultado);
-		System.out.println(nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado");
-        //Assim, os dados ficam de uma forma mais concisa.
+        int nota = 10;
+		System.out.println(nota >= 7 ? "Aprovado" : (nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado"));
 	}
 }
