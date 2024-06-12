9561e248f47947db6cd5fd2573cab8486af1ad83
Paulo Sergio <ashnikko5297@gmail.com>
2024-06-12 06:21:40 -0300

    Condicionais encandeadas, são estruturas usuais no dia a dia para multiplas opções, "if" sempre virá primeiro e "else" por último, "else if" ficam no meio dessa estrutura.

--- ContaBanco/src/ResultadoEscolar.java
+++ ContaBanco/src/ResultadoEscolar.java
@@ -1,12 +1,16 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-        int nota = 8;
+        int nota = 4;
         
-        if(nota >= 7)
+        if(nota >= 7){
          System.out.println("Aprovado");
- 
+         System.out.println("Nota: " +nota+ " parabéns!");
+
+        }else if (nota >= 5 && nota < 7)
+        System.out.println("Recuperação dia 15/02/2020.");
+
         else
-         System.out.println("Reprovado");
+         System.out.println("Recuperação dia 30/05/2020.");
      }
 }
