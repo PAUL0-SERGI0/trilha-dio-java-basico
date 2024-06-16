6284668992272f72846208e9a449a4fc47e1b526
Paulo Sergio <ashnikko5297@gmail.com>
2024-06-16 08:46:13 -0300

    For array serve para um indice de elementos e SEMPRE será iniciado com a contagem 0 ZERO.

---
+++ ContaBanco/src/EmelploForArray.java
@@ -0,0 +1,12 @@
+public class EmelploForArray {
+    public static void main(String[] args) {
+
+        //em arrays o indice de elementos inicia em ZERO
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        //Forma abreviada
+        for(String aluno : alunos) {
+        System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
---
+++ ContaBanco/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carregar = 1 ; carregar <= 100 ; carregar ++){
+            System.out.println("Carregando " + carregar);
+
+        }
+        System.out.println("Carregamento concluído com sucesso!");
+    }
+}
